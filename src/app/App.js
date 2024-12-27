2037df740e0fe6e56b24bee2e75a64a3be973bac
pluput <93170542+pluput@users.noreply.github.com>
2024-12-27 18:27:01 -0500

    getting articles to work

--- src/app/App.js
+++ src/app/App.js
@@ -22,7 +22,7 @@ export default function App() {
   const [isCursorVisible, setIsCursorVisible] = useState(true); // You can remove this if you're not using the cursor
 
   return (
-    <Router basename="/adamssite">
+    <Router basename={process.env.PUBLIC_URL}>
       <ScrollToTop>
         <Headermain />
         <AppRoutes />  {/* This is where your routes are rendered */}
